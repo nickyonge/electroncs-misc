c1d853f22ff71909ebd943b4e33b26ae729ca646
Nick Yonge <nick@curiobot.com>
2023-01-25 18:02:09 -0800

    change pin config to fit circuit

--- Nano Simple Light Bar/src/main.cpp
+++ Nano Simple Light Bar/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define HAS_ON_SWITCH true
+#define ON_SWITCH_PIN 13
+
+void setup()
+{
+
+    Serial.begin(9600);
+
+    if (HAS_ON_SWITCH)
+    {
+        pinMode(ON_SWITCH_PIN, INPUT);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
+
+    if (digitalRead(ON_SWITCH_PIN)) {
+        Serial.println("ON");
+    } else {
+        Serial.println("OFF");
+
+    }
 }
