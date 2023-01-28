6f2954d5423385a657e4fd77cca0c40b9139483c
Nick Yonge <nick@curiobot.com>
2023-01-27 20:21:40 -0800

    added some more to main.cpp

--- Nano Simple Light Bar/src/main.cpp
+++ Nano Simple Light Bar/src/main.cpp
@@ -2,26 +2,44 @@
 
 #define HAS_ON_SWITCH true
 #define ON_SWITCH_PIN 13
+#define ON_SWITCH_MODE OUTPUT
+
+#define HAS_BRIGHTNESS_POT true
+#define BRIGHTNESS_POT_PIN A2
+#define BRIGHTNESS_POT_MODE OUTPUT
+#define DEFALUT_BRIGHTNESS 400 // 0-1024, used if bright pot pin is unset
 
 void setup()
 {
 
     Serial.begin(9600);
 
-    if (HAS_ON_SWITCH)
-    {
-        pinMode(ON_SWITCH_PIN, INPUT);
-    }
+#ifdef HAS_ON_SWITCH
+    pinMode(ON_SWITCH_PIN, ON_SWITCH_MODE);
+#endif
+
+#ifdef HAS_BRIGHTNESS_POT
+    pinMode(BRIGHTNESS_POT_PIN, BRIGHTNESS_POT_MODE);
+#endif
+
 }
 
 void loop()
 {
     // put your main code here, to run repeatedly:
 
-    if (digitalRead(ON_SWITCH_PIN)) {
-        Serial.println("ON");
-    } else {
-        Serial.println("OFF");
+#ifdef HAS_ON_SWITCH
+    bool active = digitalRead(ON_SWITCH_PIN);
+#else
+    bool active = true;
+#endif
 
-    }
+    // int brightness = analogRead(BRIGHTNESS_POT_PIN);
+    int brightness = active ? analogRead(BRIGHTNESS_POT_PIN) : 0;
+
+    Serial.print("active: ");
+    Serial.println(active);
+    Serial.print("brightness: ");
+    Serial.println(brightness);
+    delay(250);
 }
