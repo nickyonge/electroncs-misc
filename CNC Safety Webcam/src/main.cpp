153bbd172104559e00bdd6d312f42d016d5105b5
Nick Yonge <nick@curiobot.com>
2023-02-10 23:22:52 -0800

    created new project for CNC machine monitoring webcam

---
+++ CNC Safety Webcam/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#if __has_include("privatedata.cpp")
+#include "privatedata.cpp"
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ CNC Safety Webcam/src/privatedata-example.cpp
@@ -0,0 +1,4 @@
+/** @brief SSID (wifi network name) */
+extern const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+/** @brief Wifi network password (leave blank if none) */
+extern const char* password = "REPLACE_WITH_YOUR_PASSWORD";
